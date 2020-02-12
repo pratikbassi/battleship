ee62d38356e08f8d585d3062209b924894cd2dc0
Pratik Bassi <pratik.bassi@mail.utoronto.ca>
2020-02-12 02:39:39 +0000

    started work on adding players

--- addPlayer.js
+++ addPlayer.js
@@ -1,9 +1,26 @@
 const makeGrid = require('./makeGrid');
 const makeShip = require('./makeShip');
+const shipPlacer = require('./shipPlacer')
 
 const addPlayer = function(){
-  let player = {}
+  let player = {
+    playerName: '',
+    playerGrid,
+    targetGrid: makeGrid(10),
+    toConsole: function(){
+      console.log(`${this.playerName}`);
+      console.log(`THIS IS THE TARGETING GRID: YOUR SHOTS APPEAR HERE`);
+      console.log(`${this.targetGrid}`);
+      console.log(`--------------------------------------------------`)
+      console.log(`THIS IS ${this.playerName}'s GRID: YOUR SHIPS AND THEIR STATUS APPEARS HERE`)
+      console.log(`${this.playerGrid}`)
+      console.log(`--------------------------------------------------`)
+    },
+    addShips: function(shipObject){
 
+    }
+  }
+  
 
 
 
---
+++ shipPlacer.js
@@ -0,0 +1,12 @@
+const shipPlacer = function(){
+  let placementData = {
+    ocean,
+    position1,
+    position2,
+    name,
+  }
+
+  
+}
+
+module.exports = shipPlacer;
